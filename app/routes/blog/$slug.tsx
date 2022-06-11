eed2bf50290e071345a547f46bbcc69cf336f01e
Lucas Vilaboim <lucas@vilaboim.com>
2022-06-11 19:08:41 -0300

    feat: improve code patterns

--- app/routes/blog/$slug.tsx
+++ app/routes/blog/$slug.tsx
@@ -1,36 +1,34 @@
 import { json } from "@remix-run/node";
+import type { LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { marked } from "marked";
-
+import type { LoaderData } from "~/types";
 import { getPost } from "~/models/blog.server";
 
-type LoaderData = {
-  post: Awaited<ReturnType<typeof getPost>>;
+interface PostLoaderData extends LoaderData<typeof getPost> {
   html: string;
 };
 
+export const loader: LoaderFunction = async ({ params }) => {
+  const data = await getPost(params.slug as string);
+  const html = marked(data.body_markdown);
 
-export const loader = async ({ params }: { params: { slug: string }}) => {
-    const post = await getPost(params.slug);
-
-  const html = marked(post.body_markdown);
-
-  return json<LoaderData>({
-    post,
+  return json({
+    data,
     html
   });
 };
 
 export default function PostSlug() {
-  const { post,html } = useLoaderData();
+  const { data, html } = useLoaderData<PostLoaderData>();
 
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">
-        {post.title}
+        {data.title}
       </h1>
       <h2 className="my-6 border-b-2 text-center text-3xl">
-        {post.tags}
+        {data.tags}
       </h2>
         <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
